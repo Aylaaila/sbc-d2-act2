cd3dd47957c2f9da326df199c49cfa48a4909c8c
Aylaaila <aila.hoylar@aclcbutuan.edu.ph>
2024-06-19 18:41:02 -0700

    Update sbc-d2-act2 (2).py

--- sbc-d2-act2 (2).py
+++ sbc-d2-act2 (2).py
@@ -1,32 +1,46 @@
 from random import randint
 
-p1 = input("kulob to hayang: ")
-c2 = randint(1,7)
-c3 = randint(1,7)
+choices0 = "Kulob"
+choices1 = "hayang"
+ran = randint(0,1)
+dan = randint(0,1)
 
-hayang = 1
-kulob = 7
-
-if p1 == kulob:
-    print("P1:kulob")
-elif p1 == hayang:
-    print("P1:hayang")
-if c2 == 7:
-    print("C2:kulob")
-elif c2 == 1:
-    print("C2:hayang")
-if c3 == 7:
-    print("C3:kulob")
-elif c3 == 1:
-    print("C3:hayang")
-    
-
-
-if p1 == hayang and c2 == 7 and c3 == 7 or p1 == kulob and c2 == 1 and c3 == 1 :
-    print("you win")
-elif p1 == 1 and c2 == 7 and c3 == 1 or p1 == 7 and c2 == 1 and c3 == 7:
-    print("c2 win")
-elif p1 == 1 and c2 == 1 and c3 == 7 or p1 == 7 and c2 == 7 and c3 == 1:
-    print("c3 win")
+user = input("Kulob or hayang >")
+if user.capitalize() == "Kulob":
+    p1 = "Kulob"
+    print("P1 = ", user)
 else:
-    print("hahay")
+    p1 = "hayang"
+    print("P1 = hayang")
+
+c2 = ran
+if c2 == 1:
+    com1 = "Kulob"
+    print("C2 = Kulob")
+else:
+    com1 = "hayang"
+    print("C2 = hayang")
+
+com2 = dan
+if com2 == 1:
+    com2 = "Kulob"
+    print("C3 = Kulob")
+else:
+    com2 = "hayang"
+    print("C3 = hayang")
+
+
+if (p1 == "Kulob" and com1 == "hayang" and com2 == "hayang") or (p1 == "hayang" and com1 == "Kulob" and com2 == "Kulob"):
+    print("P1 Win!!!!")
+
+
+elif (p1 == "hayang" and com1 == "Kulob" and com2 == "hayang") or (p1 == "Kulob" and com1 == "hayang" and com2 == "Kulob"):
+    print("C2 Win!!!!")
+
+
+elif (p1 == "hayang" and com1 == "hayang" and com2 == "Kulob") or (p1 == "Kulob" and com1 == "Kulob" and com2 == "hayang"):
+    print("C3 Win!!!!")
+
+
+else:
+    print("naahh")
